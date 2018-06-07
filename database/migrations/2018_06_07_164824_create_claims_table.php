d01453621f38ad211676216a756475e4f0c69535
Andrew Erwin <andrew@erwininteractive.com>
2018-06-07 12:10:08 -0500

    Initial commit

---
+++ database/migrations/2018_06_07_164824_create_claims_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClaimsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('claims', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('opportunity_id');
+            $table->float('value', 10, 2);
+            $table->text('customer_name');
+            $table->text('country');
+            $table->enum('status', ['PENDING', 'APPROVED', 'DENIED']);
+            $table->date('activity_date');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('claims');
+    }
+}
